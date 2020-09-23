8b2335f729462f1951fb8e75d72ff459da081c7b
Dmitry Sorokin <redballtoy@gmail.com>
2020-09-23 14:06:47 +0300

    add url_launcher: ^3.0.1

--- lib/MyBody.dart
+++ lib/MyBody.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MyBody extends StatelessWidget{
 
@@ -8,9 +9,16 @@ class MyBody extends StatelessWidget{
     return new Center(child: new Column(
         children: [
             new Text('Hello world'),
-          new FlatButton(onPressed: (){}
-                        , child: Text('open site')
-                        , color: Colors.red
+          new FlatButton(onPressed: () async{
+                                              const url='https://flutter.su';
+                                              if(await canLaunch(url)){
+                                                await launch(url);
+                                              }else{
+                                                throw 'Could not launch $url';
+                                              }
+                                            }
+                        , child: Text('open Flutter')
+                        , color: Colors.blue
                         ,textColor: Colors.white,)
         ]
     ));
